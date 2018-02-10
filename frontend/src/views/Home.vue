dd5543321e9d925fbada00ee1a430fbda4c0d227
Fabien Loffredo <contact@fabienloffredo.com>
2018-02-10 14:09:35 +0100

    Add simple info message

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,20 +1,13 @@
 <template>
   <b-container>
-    Home
+    <b-alert variant="warning" show>
+      Site en cours de construction. La base est vouée à être écrasée régulièrement, ne vous embêtez pas à remplir tous les pronos. Merci pour vos retours et votre temps !!
+    </b-alert>
   </b-container>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  data () {
-    return {
-    }
-  },
-  computed: {
-  }
+  name: 'Home'
 }
 </script>
-
-<style scoped>
-</style>
